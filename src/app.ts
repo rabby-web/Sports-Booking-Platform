7d7f3345620064c82cf73b8975b484bae8aeab4c
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-03 21:05:21 +0600

    global routes add

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
-import express, { Request, Response } from 'express';
-const app = express();
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import router from './app/routes';
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Project Sports Booking Platform');
+const app: Application = express();
+app.use(cors());
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
+
+//Testing
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: 'Welcome to the Lost And Found API',
+  });
 });
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Project Sports Booking Platform');
+// });
 
 export default app;
