cf3aa3079cf6e3d383fb0d003da7e8610eea3e38
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-06 22:22:43 +0600

    route change to v1 cancel of route

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', router);
+app.use('/api', router);
 
 //Testing
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
---
+++ src/app/modules/Facility/facility.route.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+export const UserRoutes = router;
+
+router.post(
+  '/facility',
+  //   validateRequest(UserValidation.createUserValidationSchema),
+  //   UserControllers.userRegister,
+);
