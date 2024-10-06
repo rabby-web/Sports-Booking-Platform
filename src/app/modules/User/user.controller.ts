c2f0644a1fa96136cd153657f806b8450b691bd6
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-06 22:47:27 +0600

    facility controller add

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -1,19 +1,19 @@
-// import httpStatus from 'http-status';
-// import { catchAsync } from '../../../utils/catchAsync';
-// import sendResponse from '../../../utils/sendResponse';
-// import { UserServices } from './user.service';
+import httpStatus from 'http-status';
+import { catchAsync } from '../../../utils/catchAsync';
+import sendResponse from '../../../utils/sendResponse';
+import { UserServices } from './user.service';
 
-// const userRegister = catchAsync(async (req, res) => {
-//   const user = await UserServices.createUser(req.body);
+const userRegister = catchAsync(async (req, res) => {
+  const user = await UserServices.createUser(req.body);
 
-//   sendResponse(res, {
-//     success: true,
-//     statusCode: httpStatus.OK,
-//     message: 'User Created Successfully',
-//     data: user,
-//   });
-// });
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: 'User Created Successfully',
+    data: user,
+  });
+});
 
-// export const UserControllers = {
-//   userRegister,
-// };
+export const UserControllers = {
+  userRegister,
+};
