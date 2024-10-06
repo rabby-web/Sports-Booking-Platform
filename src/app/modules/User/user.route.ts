8bb6bd59613041b48245aa193c17a14f2fd497be
rabby-web <rabby.webdeveloper@gmail.com>
2024-10-06 22:00:36 +0600

    user validation

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { UserControllers } from './user.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
 export const UserRoutes = router;
 
-router.post('/create-user', UserControllers.userRegister);
+router.post(
+  '/create-user',
+  validateRequest(UserValidation.createUserValidationSchema),
+  UserControllers.userRegister,
+);
--- src/app/modules/User/user.validation.ts
+++ src/app/modules/User/user.validation.ts
@@ -1,39 +1,39 @@
-// import { z } from 'zod';
-// import { USER_ROLE } from './user.constant';
+import { z } from 'zod';
+import { USER_ROLE } from './user.constant';
 
-// const createUserValidationSchema = z.object({
-//   body: z.object({
-//     name: z.string({
-//       required_error: 'Name is required',
-//     }),
-//     role: z.nativeEnum(USER_ROLE),
-//     email: z
-//       .string({
-//         required_error: 'Email is required',
-//       })
-//       .email({
-//         message: 'Invalid email',
-//       }),
-//     password: z.string({
-//       required_error: 'Password is required',
-//     }),
-//     phone: z.string().optional(),
-//     address: z.string().optional(),
-//   }),
-// });
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      required_error: 'Name is required',
+    }),
+    role: z.nativeEnum(USER_ROLE),
+    email: z
+      .string({
+        required_error: 'Email is required',
+      })
+      .email({
+        message: 'Invalid email',
+      }),
+    password: z.string({
+      required_error: 'Password is required',
+    }),
+    phone: z.string().optional(),
+    address: z.string().optional(),
+  }),
+});
 
-// const updateUserValidationSchema = z.object({
-//   body: z.object({
-//     name: z.string().optional(),
-//     role: z.nativeEnum(USER_ROLE).optional(),
-//     email: z.string().email().optional(),
-//     password: z.string().optional(),
-//     phone: z.string().optional(),
-//     address: z.string().optional(),
-//   }),
-// });
+const updateUserValidationSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    role: z.nativeEnum(USER_ROLE).optional(),
+    email: z.string().email().optional(),
+    password: z.string().optional(),
+    phone: z.string().optional(),
+    address: z.string().optional(),
+  }),
+});
 
-// export const UserValidation = {
-//   createUserValidationSchema,
-//   updateUserValidationSchema,
-// };
+export const UserValidation = {
+  createUserValidationSchema,
+  updateUserValidationSchema,
+};
